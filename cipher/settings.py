93660070a27b9ff4c43f916c185aafd19a7009a4
Rushit2907 <54739553+Rushit2907@users.noreply.github.com>
2021-05-10 19:24:50 +0530

    Update settings.py

--- cipher/settings.py
+++ cipher/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-s+d(egrnqbxyl!$+t6^zzs^y3=y07+o_v9rzgqkeuonxfd1ucz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
